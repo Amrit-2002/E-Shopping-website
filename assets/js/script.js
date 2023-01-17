1cc32b20aa4f63ff94f5b77504a52039f41dc635
Amrit Kumar Maurya <92193846+Amrit-2002@users.noreply.github.com>
2023-01-18 00:55:55 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
 let countDown = () => {
-    let futureDate = new Date("30 November 2022");
+    let futureDate = new Date("30 May 2023");
     let currentDate = new Date();
     let myDate = futureDate - currentDate;
 
